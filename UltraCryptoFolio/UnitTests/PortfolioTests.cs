644ea3832ccc8b3a90030d0b4905b5750a2eb9b7
Joost van Uitert <joostvanuitert@gmail.com>
2018-01-06 10:54:20 +0100

    Unit test project

---
+++ UltraCryptoFolio/UnitTests/PortfolioTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using UltraCryptoFolio.Models;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<Transaction> transactions = new List<Transaction>()
+            {
+                new Investment()
+                {
+                    
+                }
+            };
+        }
+    }
+}

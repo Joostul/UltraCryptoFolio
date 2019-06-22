39d6f7b1c1ed307b2a032eff89a62e12abd4c405
Joost van Uitert <joostvanuitert@gmail.com>
2019-06-22 21:15:26 +0200

    Update gitigore Get storage account nuget

---
+++ UltraCryptoFolio/Repositories/StoragePortfolioRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UltraCryptoFolio.Models;
+using UltraCryptoFolio.Models.Enums;
+
+namespace UltraCryptoFolio.Repositories
+{
+    public class StoragePortfolioRepository : IPortfolioRepository
+    {
+
+        public StoragePortfolioRepository()
+        {
+
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<Currency, decimal> GetCurrenciesWorth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetCurrencyWorth(Currency currency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetTotalWorth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaction GetTransaction(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Transaction> GetTransactions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
